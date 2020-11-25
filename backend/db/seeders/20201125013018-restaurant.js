64471f73c46405fb772d6625dca1a86ba715f3a1
carlos molero <cmolero130@gmail.com>
2020-11-24 21:23:15 -0500

    add seeder

--- backend/db/seeders/20201125013018-restaurant.js
+++ backend/db/seeders/20201125013018-restaurant.js
@@ -1,26 +1,58 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            "Restaurants",
+            [
+                {
+                    name: "The Chum Bucket",
+                    location: "Bikini Bottom",
+                    cuisine: "Burgers",
+                    takeOut: "true",
+                    restaurantImage: "/backend/public/imgs/chumbucket.jpeg",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "The Krusty Krab",
+                    location: "Bikini Bottom",
+                    cuisine: "Burgers",
+                    takeOut: "false",
+                    restaurantImage: "/backend/public/imgs/krustykrab.jpeg",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "City Wok",
+                    location: "Colorado Springs",
+                    cuisine: "Asian fuision",
+                    takeOut: "true",
+                    restaurantImage: "/backend/public/imgs/citywok.jpeg",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+                {
+                    name: "Druken Clam",
+                    location: "Quahog",
+                    cuisine: "Burgers",
+                    takeOut: "false",
+                    restaurantImage: "/backend/public/imgs/chumbucket.jpeg",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-
-     
-  }
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        return queryInterface.bulkDelete("Resturants", null, {});
+    },
 };
